b1c56dca0ce6755c46ff0782e5c5ac6749e39830
GustavoGazzinelli <125841672+GustavoGazzinelli@users.noreply.github.com>
2026-02-25 08:01:41 -0300

    Add files via upload

---
+++ src/types/pokemon.ts
@@ -0,0 +1,13 @@
+export type PokemonLite = {
+    id: number
+    nome: string
+    tipo1: string
+    tipo2: string
+    gen: number
+    fase: number
+    cores: string[]
+    habitat: string
+    altura: number
+    peso: number
+    bst: number
+  }
